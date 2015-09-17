a21d0da8c68a6732104dd71c119f9342a44aa0d6
Mauro Gestoso <maurogestoso@gmail.com>
2015-09-17 16:06:53 +0100

    Add physics and speed constants

--- assets/js/Game.js
+++ assets/js/Game.js
@@ -1,7 +1,14 @@
 MyGame.Game = function(game) {};
 MyGame.Game.prototype = {
 
-  init: function() {},
+  init: function() {
+
+    this.physics.startSystem(Phaser.Physics.ARCADE);
+    this.physics.arcade.gravity.y = 1000;
+
+    this.RUNNING_SPEED = 180;
+    this.JUMPING_SPEED = 550;
+  },
 
   create: function() {
 
