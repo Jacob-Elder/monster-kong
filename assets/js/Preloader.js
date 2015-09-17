33f7374d6fb0054a355f26c907f807c51466aed3
Mauro Gestoso <maurogestoso@gmail.com>
2015-09-17 20:32:40 +0100

    Add Boot state

--- assets/js/Preloader.js
+++ assets/js/Preloader.js
@@ -1,20 +1,7 @@
-var MyGame = {
-
-};
-
-MyGame.Preloader = function(game) {
-
-};
+MyGame.Preloader = function(game) {};
 
 MyGame.Preloader.prototype = {
-  init: function() {
-
-    // this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    this.scale.pageAlignHorizontally = true;
-    this.scale.pageAlignVertically = true;
-
-  },
-
+  
   preload: function() {
 
     this.load.path = "assets/";
