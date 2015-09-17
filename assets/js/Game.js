3ee49b170b0af95a46cc31ceac7d0af8d3ee4134
Mauro Gestoso <maurogestoso@gmail.com>
2015-09-17 17:15:45 +0100

    Set camera to follow player. Fix to camera onscreen buttons.

--- assets/js/Game.js
+++ assets/js/Game.js
@@ -6,13 +6,15 @@ MyGame.Game.prototype = {
     this.physics.startSystem(Phaser.Physics.ARCADE);
     this.physics.arcade.gravity.y = 1000;
 
+    this.world.setBounds(0, 0, 360, 700);
+
     this.RUNNING_SPEED = 180;
     this.JUMPING_SPEED = 550;
   },
 
   create: function() {
 
-    this.ground = this.add.sprite(0, 520, "ground");
+    this.ground = this.add.sprite(0, 628, "ground");
     this.physics.arcade.enable(this.ground);
     this.ground.body.allowGravity = false;
     this.ground.body.immovable = true;    
@@ -34,11 +36,14 @@ MyGame.Game.prototype = {
     this.platforms.setAll("body.allowGravity", false);
     
     // player
-    this.player = this.add.sprite(100, 200, "player", 3);
+    this.player = this.add.sprite(10, 545, "player", 3);
     this.player.anchor.setTo(0.5);
     this.player.animations.add("walking", [0, 1, 2, 1], 6, true);
     this.physics.arcade.enable(this.player);
     this.player.customParams = {};
+
+    // camera
+    this.camera.follow(this.player);
 
     // Controls
     this.cursors = this.input.keyboard.createCursorKeys();
@@ -73,12 +78,15 @@ MyGame.Game.prototype = {
 
     this.leftArrow = this.add.button(20, 535, "arrowButton");
     this.leftArrow.alpha = 0.5;
+    this.leftArrow.fixedToCamera = true;
 
     this.rightArrow = this.add.button(110, 535, "arrowButton");
     this.rightArrow.alpha = 0.5;
+    this.rightArrow.fixedToCamera = true;
 
     this.actionButton = this.add.button(280, 535, "actionButton");
     this.actionButton.alpha = 0.5;
+    this.actionButton.fixedToCamera = true;
 
     // left
     this.leftArrow.events.onInputDown.add(function() {
