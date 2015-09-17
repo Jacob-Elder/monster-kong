6e7afe9216344cef0ddfcb9ae1411b1acbc0b74d
Mauro Gestoso <maurogestoso@gmail.com>
2015-09-17 16:05:44 +0100

    Load assets

--- assets/js/Preloader.js
+++ assets/js/Preloader.js
@@ -19,6 +19,16 @@ MyGame.Preloader.prototype = {
 
     this.load.path = "assets/";
 
+    this.load.image("ground", "img/ground.png");
+    this.load.image("platform", "img/platform.png");
+    this.load.image("arrowButton", "img/arrowButton.png");
+    this.load.image("actionButton", "img/actionButton.png");
+    this.load.image("barrel", "img/barrel.png");
+    this.load.image("gorilla", "img/gorilla3.png");
+
+    this.load.spritesheet("player", "img/player_spritesheet.png", 28, 30, 5, 1, 1);
+    this.load.spritesheet("fire", "img/player_spritesheet.png", 20, 21, 2, 1, 1);
+
   },
 
   create: function() {
