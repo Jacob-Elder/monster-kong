125fd3669774e580c499280e8b3a6ff236d4ccd2
Mauro Gestoso <maurogestoso@gmail.com>
2015-09-29 11:47:18 +0100

    Turn off scaleMode

--- assets/js/Boot.js
+++ assets/js/Boot.js
@@ -5,7 +5,7 @@ MyGame.Boot.prototype = {
 
   init: function() {
 
-    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    // this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
 
