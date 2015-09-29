c80672fdfe4c865fd7af52131eb6f3291ac5a0ba
Mauro Gestoso <maurogestoso@gmail.com>
2015-09-29 12:15:56 +0100

    Fix assets names and paths

--- assets/js/Preloader.js
+++ assets/js/Preloader.js
@@ -11,10 +11,10 @@ MyGame.Preloader.prototype = {
     this.load.image("arrowButton", "img/arrowButton.png");
     this.load.image("actionButton", "img/actionButton.png");
     this.load.image("barrel", "img/barrel.png");
-    this.load.image("gorilla", "img/gorilla3.png");
+    this.load.image("goal", "img/gorilla3.png");
 
     this.load.spritesheet("player", "img/player_spritesheet.png", 28, 30, 5, 1, 1);
-    this.load.spritesheet("fire", "img/player_spritesheet.png", 20, 21, 2, 1, 1);
+    this.load.spritesheet("fire", "img/fire_spritesheet.png", 20, 21, 2, 1, 1);
 
     // this.load.json("level", "data/level.json"); 
     this.load.text("level", "data/level.json");
